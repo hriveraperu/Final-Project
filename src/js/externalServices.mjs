14800762344c159ab15990a5a23d2019d7269c44
hriveraperu <gioxshaoran@hotmail.com>
2023-07-07 15:16:30 -0500

    =)

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -37,20 +37,14 @@ export async function loginRequest(user) {
   
   console.log(baseURL);
   try {
-    const response = await fetch(baseURL + "login", {
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify(user), // body data type must match "Content-Type" header
-    });
-  
+    const response = await fetch(baseURL + "login");
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+
+    console.log(response);
+    
+
   return response.json();
   } catch(err) {
     console.log(err);
--- src/js/login.js
+++ src/js/login.js
@@ -1,19 +1,38 @@
-import { getParam } from "./utils.mjs";
-import { login } from "./auth.mjs";
 
+// let url = getParam("redirect");
 
-let url = getParam("redirect");
+import { application } from "express";
 
 const loginBtn = document.querySelector(".login");
 //console.log("sup");
 
 
-loginBtn.addEventListener("click", () => {
+loginBtn.addEventListener("click", (e) => {
   //console.log("event listened");
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
-  let creds = { email, password };
-
-  login(creds, url);
+  e.preventDefault();
+  const response = fetch ("https://reg-and-login-form.onrender.com/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({email: email, password: password})
 });
+  response.then((response) => response.json())
+  .then((result) => {
+    if(result.message === "SUCCESS"){
+      alert("You are logged in.");
+      
+     } else {
+         alert("Please check your login information.");
+         console.log(result);
+     }
+  });
+});
+  
+//   let creds = { email, password };
+
+//   login(creds, url);
+// });
 
